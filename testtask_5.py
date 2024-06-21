9759777540558b7ee5203058d47e878b01b69257
mahmoud adel <30966854+mahmoud0x01@users.noreply.github.com>
2024-06-21 16:30:36 +0700

    Update testtask_5.py

--- testtask_5.py
+++ testtask_5.py
@@ -49,17 +49,39 @@ class TestLongestConsecutiveSequence(unittest.TestCase):
     def test_negative_numbers(self):
         self.assertEqual(self.solution.longestConsecutive([-1, -2, -3, -4, 0, 1, 2, 3, 4]), 9)
 
+    # Additional tests
+    def test_all_negative_numbers(self):
+        self.assertEqual(self.solution.longestConsecutive([-10, -9, -8, -7, -6]), 5)
+
+    def test_single_negative_number(self):
+        self.assertEqual(self.solution.longestConsecutive([-1]), 1)
+
+    def test_large_negative_to_positive_range(self):
+        self.assertEqual(self.solution.longestConsecutive(list(range(-1000, 1001))), 2001)
+
+    def test_mixture_of_positive_negative_and_zero(self):
+        self.assertEqual(self.solution.longestConsecutive([-1, 0, 1, 2, -2, -3, 3, 4, 5]), 9)
+
+    def test_already_sorted_list(self):
+        self.assertEqual(self.solution.longestConsecutive([1, 2, 3, 4, 5]), 5)
+
+    def test_random_order_elements(self):
+        self.assertEqual(self.solution.longestConsecutive([10, 6, 2, 3, 5, 8, 9, 1, 4, 7]), 10)
+
     def test_all_elements_same(self):
         self.assertEqual(self.solution.longestConsecutive([5, 5, 5, 5, 5]), 1)
 
     def test_large_range(self):
-        self.assertEqual(self.solution.longestConsecutive([-50, -49, 50, 51, 52, 53, 54, 55, -48, -47, 0, 1, 2, 3]), 9)
+        self.assertEqual(self.solution.longestConsecutive(list(range(-50, 51))), 101)
 
     def test_gaps_in_sequence(self):
         self.assertEqual(self.solution.longestConsecutive([10, 1, 3, 5, 2, 4, 6, 100, 101, 102, 103, 104]), 6)
 
     def test_large_input(self):
         self.assertEqual(self.solution.longestConsecutive(list(range(1000000))), 1000000)
+
+    def test_unsorted_list(self):
+        self.assertEqual(self.solution.longestConsecutive([10, 5, 12, 3, 55, 7, 6, 8, 4, 11]), 6)
 
 if __name__ == '__main__':
     unittest.main()
